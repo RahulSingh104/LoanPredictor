33f0589dfb9a16a5a6a13a1595c229f0692ac63e
Rahul kumar <rahulsingh96287@gmail.com>
2025-08-25 12:22:50 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -325,4 +325,5 @@ def _server(e):
 # Main
 # -------------------------------
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5001)) 
     app.run(host="0.0.0.0", port=5001, debug=True)
